7cb44249d7f9793c75d34d43db66f52338d25c44
Menghui Wang <wmhkebe@gmail.com>
2014-01-27 17:03:17 -0600

    statistics

--- src/agent.cc
+++ src/agent.cc
@@ -1,44 +1,72 @@
+#include <iostream>
+
 #include "agent.h"
 
 using namespace std;
 
-int Agent::search(Board &board, int side, Move *result)
+int Agent::search(Board &board, int side, MOVE *result)
 {
-    return alpha_beta(board, side, result, 5, -INF, INF);
+    firstHit = 0;
+    secondHit = 0;
+    miss = 0;
+
+    int ret = alpha_beta(board, side, result, 5, -INF, INF, true);
+
+    int tot = firstHit + secondHit + miss;
+    cout << (double) firstHit / (double) tot << " " << (double) secondHit / (double) tot << " " << (double) miss / (double) tot << endl;
+    return ret;
 }
 
-int Agent::alpha_beta(Board &board, int side, Move *result, int depth, int alpha, int beta)
+int Agent::alpha_beta(Board &board, int side, MOVE *result, int depth, int alpha, int beta, bool nullable)
 {
     int ans = -INF;
+
     if (depth == 0)
         ans = board.static_value(side);
     else
     {
-        Move moves[120];
-        int moves_count;
-        board.generate_moves(side, moves, &moves_count);
-
-        for (int i = 0; i < moves_count; ++i)
+        MOVE best_move;
+        // Null-move
+        if (nullable)
         {
-            Move move = moves[i];
-            if (!board.move(move))
-                continue;
+            ans = -alpha_beta(board, 1 - side, &best_move, depth - 1, -beta, -alpha, false);
+        }
 
-            int current_alpha = max(alpha, ans);
-            Move best_move;
-            int t = -alpha_beta(board, 1 - side, &best_move, depth - 1, -beta, -current_alpha);
-            board.unmove();
+        if (ans < beta)
+        {
+            MOVE moves[120];
+            int moves_count;
+            board.generate_moves(side, moves, &moves_count);
 
-            if (t > ans)
+            int bestIndex = 0;
+            for (int i = 0; i < moves_count; ++i)
             {
-                ans = t;
-                *result = move;
-            }
+                MOVE move = moves[i];
+                if (!board.move(move))
+                    continue;
 
-            if (t >= beta)
-            {
-                break;
+                int current_alpha = max(alpha, ans);
+                int t = -alpha_beta(board, 1 - side, &best_move, depth - 1, -beta, -current_alpha, true);
+                board.unmove();
+
+                if (t > ans)
+                {
+                    bestIndex = i;
+                    ans = t;
+                    *result = move;
+                }
+
+                if (t >= beta)
+                {
+                    break;
+                }
             }
+            if (bestIndex == 0)
+                ++firstHit;
+            else if (bestIndex == 1)
+                ++secondHit;
+            else
+                ++miss;
         }
     }
 
--- src/agent.h
+++ src/agent.h
@@ -5,10 +5,13 @@
 class Agent
 {
     public:
-        int search(Board &board, int side, Move *result);
+        int search(Board &board, int side, MOVE *result);
 
         static const int INF = 0xfffffff;
 
     protected:
-        int alpha_beta(Board &board, int side, Move *result, int depth, int alpha, int beta);
+        int alpha_beta(Board &board, int side, MOVE *result, int depth, int alpha, int beta, bool nullable);
+
+        int firstHit, secondHit, miss;
+        int move_score[65535];
 };
