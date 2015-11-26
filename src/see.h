387634cd650497fa0c2dc8acc5b8671a92fed07d
Menghui Wang <wmhkebe@gmail.com>
2015-11-26 13:23:16 -0800

    Clean up everything!

--- src/see.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "board.h"
-
-bool is_winning_capture(Board *board, MOVE move, int score, int side);
-bool is_winning_capture(Board *board, MOVE move, int side);
-
-int static_exchange_eval(Board *board, int side, POSITION pos);
