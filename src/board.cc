30ff3db4c4181dac6565cf734a3b721863b87e84
Menghui Wang <wmhkebe@gmail.com>
2016-05-31 00:36:03 -0400

    passed first board_test

--- src/board.cc
+++ src/board.cc
@@ -1,8 +1,11 @@
 #include <cassert>
+#include <iostream>
 #include <map>
 
 #include "board.h"
 
+using std::cout;
+using std::endl;
 using std::map;
 using std::string;
 using std::vector;
@@ -28,16 +31,16 @@ bool Board::SetBoard(const string& fen) {
     }
   }
 
-  int row = 0, col = 0;
+  int row = 9, col = 0;
   bool rk_found = false, bk_found = false;
   for (char c : fen) {
     if (c >= '0' && c <= '9') {
       col += (c - '0');
-      if (col >= kNumColumns) return false;
     } else if (c == '/') {
       if (col != kNumColumns) return false;
-      ++row;
-      if (row >= kNumRows) return false;
+      --row;
+      col = 0;
+      if (row < 0) return false;
     } else {
       if (char_piece_map.count(c) == 0) return false;
       if (char_piece_map[c] == Piece(Side::kRed, PieceType::kKing)) {
@@ -47,10 +50,11 @@ bool Board::SetBoard(const string& fen) {
         if (!(row >= 7 && col >= 3 && col <= 5)) return false;
         bk_found = true;
       }
+      if (col >= kNumColumns) return false;
       ++col;
     }
   }
-  if (row != kNumRows - 1) return false;
+  if (row != 0) return false;
   if (col != kNumColumns) return false;
   if (!rk_found || !bk_found) return false;
 
@@ -63,12 +67,13 @@ bool Board::SetBoard(const string& fen) {
     piece_bitboards_[i] = BitBoard::EmptyBoard();
   }
 
-  row = 0, col = 0;
+  row = 9, col = 0;
   for (char c : fen) {
     if (c >= '0' && c <= '9') {
       col += (c - '0');
     } else if (c == '/') {
-      ++row;
+      --row;
+      col = 0;
     } else {
       Piece piece = char_piece_map[c];
       board_[row][col] = piece;
@@ -90,7 +95,7 @@ string Board::ToString() const {
       gap = 0;
     }
   };
-  for (int i = 0; i < kNumRows; ++i) {
+  for (int i = kNumRows - 1; i >= 0; --i) {
     for (int j = 0;j < kNumColumns; ++j){ 
       if (board_[i][j] != Piece::EmptyPiece()) {
         append_gap();
@@ -100,7 +105,7 @@ string Board::ToString() const {
       }
     }
     append_gap();
-    if (i < kNumRows - 1) ans += "/";
+    if (i != 0) ans += "/";
   }
   return ans;
 }
