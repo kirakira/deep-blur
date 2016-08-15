1232d769a4de8f19c0a9090fb4a7f7f3a229301d
Menghui Wang <wmhkebe@gmail.com>
2016-08-15 00:22:24 -0700

    forgot to add source files

---
+++ src/logger.cc
@@ -0,0 +1,19 @@
+#include "logger.h"
+
+namespace blur {
+
+void Logger::Print() const {
+  using std::cout;
+  using std::endl;
+  for (const auto& node : nodes_) {
+    cout << "node " << node.id << " parent " << node.parent_id << " from_move "
+         << node.from_move.ToString() << " "
+         << (node.side == Side::kRed ? "r" : "b") << " depth " << node.depth
+         << " alpha " << node.alpha << " beta " << node.beta << " final "
+         << node.score << " " << node.best_move.ToString() << endl;
+  }
+}
+
+void Logger::SaveNode(const SearchNodeInfo& info) { nodes_.push_back(info); }
+
+}  // namespace blur
---
+++ src/logger.h
@@ -0,0 +1,54 @@
+#ifndef BLUR_LOGGER_H
+#define BLUR_LOGGER_H
+
+#include <iostream>
+#include <vector>
+
+#include "board-base.h"
+#include "common.h"
+#include "eval.h"
+
+namespace blur {
+
+class Logger {
+ public:
+  struct SearchNodeInfo {
+    SearchNodeInfo(int64 current_id, int64 parent, Move parent_move,
+                   Side current_side, int current_depth, Score initial_alpha,
+                   Score initial_beta)
+        : id(current_id),
+          parent_id(parent),
+          from_move(parent_move),
+          side(current_side),
+          depth(current_depth),
+          alpha(initial_alpha),
+          beta(initial_beta) {}
+    int64 id;
+    int64 parent_id;
+    Move from_move;
+    Side side;
+    int depth;
+    Score alpha;
+    Score beta;
+    Score score;
+    Move best_move;
+  };
+
+  void SaveNode(const SearchNodeInfo& info);
+
+  void Print() const;
+
+  static Logger* GetInstance() {
+    static Logger* logger = new Logger();
+    return logger;
+  }
+
+ private:
+  Logger() = default;
+
+  std::vector<SearchNodeInfo> nodes_;
+};
+
+}  // namespace blur
+
+#endif  // BLUR_LOGGER_H
